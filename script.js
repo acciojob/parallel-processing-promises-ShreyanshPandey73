5baf79155cb1bdcb6b4fed7ab11be50174c82286
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-13 21:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+ function displayImages(images) {
+      const outputDiv = document.getElementById('output');
+      images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.src;
+        outputDiv.appendChild(imgElement);
+      });
+    }
+
+    // Event listener for the "Download Images" button
+    document.getElementById('download-images-button').addEventListener('click', () => {
+      // Use Promise.all to download all images in parallel
+      Promise.all(images.map(downloadImage))
+        .then(images => {
+          // Call the displayImages function to display the downloaded images
+          displayImages(images);
+        })
+        .catch(error => {
+          // Display error message if any image fails to download
+          console.error(error);
+        });
+    });
